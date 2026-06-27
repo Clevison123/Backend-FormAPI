ec49068b9d7bd2219f29fdcd6613416f1e5fff3e
José Clévison <cdefensor467@gmail.com>
2026-06-27 17:48:58 -0300

    Allow Netlify frontend in CORS

--- Program.cs
+++ Program.cs
@@ -23,7 +23,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://frontend-formapi.netlify.app"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
